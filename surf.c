57cee7c81991be578b81fe82d2a61fb69c2a15c5
Lars Kiesow <lkiesow@uos.de>
2013-01-01 15:24:13 +0100

    Added command line option for filterfile

--- surf.c
+++ surf.c
@@ -1644,9 +1644,12 @@ updatewinid(Client *c)
 void
 usage(void)
 {
-	die("usage: %s [-bBdDfFgGiIkKmMnNpPsSvx] [-a cookiepolicies ] "
-	    "[-c cookiefile] [-e xid] [-r scriptfile] [-t stylefile] "
-	    "[-u useragent] [-z zoomlevel] [uri]\n", basename(argv0));
+	die("usage: %s [-bBdDfFgGiIkKmMnNpPsSvx0]"
+		" [-a cookiepolicies ] "
+		" [-c cookiefile] [-e xid] [-r scriptfile]"
+		" [-t stylefile] [-u useragent] [-z zoomlevel]"
+		" [-0 filterile]"
+		" [uri]\n", basename(argv0));
 }
 
 void
@@ -1864,6 +1867,8 @@ main(int argc, char *argv[])
 	case 'z':
 		zoomlevel = strtof(EARGF(usage()), NULL);
 		break;
+	case '0':
+		filterfile = EARGF(usage());
 	default:
 		usage();
 	} ARGEND;
